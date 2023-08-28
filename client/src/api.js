0de6112c5e31b0bde4b790a0763c28044e5a659a
Abhishek Singh Sengar <theabhissengar@gmail.com>
2023-08-28 07:51:07 +0530

    done

---
+++ client/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93f709734dmshca5c45ae4226cb7p18b737jsnb182dd7e8419",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "25f38d56f46a55d0a46fd3664392b6f6"; // enter your key from openweather API
---
+++ client/src/components/Weather/Weather.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+
+const Weather = ({ data }) => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city.toUpperCase()}</p><br/>
+          <p className="weather-description">{data.weather[0].description.toUpperCase()}</p>
+        </div>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
+      </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label"><b>DETAILS</b></span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">TEMPERATURE</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">WIND</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">HUMIDITY</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">PRESSURE</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
